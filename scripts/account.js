daa2cf181f5da88fc03bf47fe2e69efefb1afc6f
stefu <stefanos.thomas@metropolia.fi>
2024-04-25 17:44:24 +0300

    form for editing profile ready. Functionality not implemented yet

--- scripts/account.js
+++ scripts/account.js
@@ -1,24 +1,26 @@
 'use strict';
 
+const createUserData = (user) => {
+    editForm.firstName.placeholder = user.first_name;
+    editForm.lastName.placeholder = user.last_name;
+    editForm.email.placeholder = user.email;
+    editForm.phone.placeholder = user.phone;
+    editForm.address.placeholder = user.address;
+    editForm.password.placeholder = "********";
+    editForm.confirmPassword.placeholder = "********";
+}
+const editForm = document.querySelector('.edit-form');
 const user = JSON.parse(sessionStorage.getItem('user'));
-const userInformation = document.getElementById('user-information');
+
 if (!user) {
     window.location.href = 'index.html';
 } else {
-    console.log(user);
-    const firstName = document.createElement('p');
-    firstName.textContent = `First Name: ${user.first_name}`;
-    const lastName = document.createElement('p');
-    lastName.textContent = `Last Name: ${user.last_name}`;
-    const email = document.createElement('p');
-    email.textContent = `Email: ${user.email}`;
-    const address = document.createElement('p');
-    address.textContent = `Address: ${user.address}`;
-    const phoneNumber = document.createElement('p');
-    phoneNumber.textContent = `Phone Number: ${user.phone}`;
-    userInformation.appendChild(firstName);
-    userInformation.appendChild(lastName);
-    userInformation.appendChild(email);
-    userInformation.appendChild(address);
-    userInformation.appendChild(phoneNumber);
+    createUserData(user);
 }
+
+const saveButton = document.querySelector('#save-button');
+
+saveButton.addEventListener('click', async (e) => {
+    e.preventDefault()
+    //TODO handle form validation
+})
