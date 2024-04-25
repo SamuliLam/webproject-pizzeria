826258fc5dddccc499507f7ec2ca3658e36230ff
stefu <stefanos.thomas@metropolia.fi>
2024-04-25 14:26:38 +0300

    Implemented sign in button and also created account page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+const signupButton = document.getElementById('login-button')
+const logOutButton = document.getElementById('logout-button')
+const accountButton = document.getElementById('account-button')
+
+if (sessionStorage.getItem('token')) {
+    signupButton.style.display = 'none'
+    logOutButton.style.display = 'block'
+    accountButton.style.display = 'block'
+}
+
+logOutButton.addEventListener('click', () => {
+    sessionStorage.clear()
+    signupButton.style.display = 'block'
+    logOutButton.style.display = 'none'
+    accountButton.style.display = 'none'
+})
+
 const hamburgerIcon = document.getElementById('hamburger-icon')
 const sidebar = document.getElementById('side-bar')
 hamburgerIcon.addEventListener('click', () => {
