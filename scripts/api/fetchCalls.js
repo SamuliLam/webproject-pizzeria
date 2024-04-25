1a0ca94dbe0a8884aaa6ccff7c815dd78f1195ed
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-04-25 13:49:59 +0300

    admin panel updates

---
+++ scripts/api/fetchCalls.js
@@ -0,0 +1,38 @@
+export const getOrders = async () => {
+    try {
+        const response = await fetch('http://10.120.32.55/app/api/v1/orders');
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data)
+            return data;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getProducts = async () => {
+    try {
+        const response = await fetch('http://10.120.32.55/app/api/v1/products');
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data)
+            return data;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getUserById = async (id) => {
+    try {
+        const response = await fetch(`http://10.120.32.55/app/api/v1/users/${id}`);
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data)
+            return data;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
