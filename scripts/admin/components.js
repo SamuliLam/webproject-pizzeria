632d99912fe0c0649992e34e07e57db4e9ba003a
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-04-30 13:19:58 +0300

    added container that appears for the order when clicking the order row. The order row displays the ordered items.

--- scripts/admin/components.js
+++ scripts/admin/components.js
@@ -86,6 +86,21 @@ function createTable(orders, keys, title, headers) {
     return table;
 }
 
+
+export const menuComponent = (menu) => {
+    const table = document.createElement("table");
+    table.classList.add("menu-table");
+
+    const thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    const headerRow = document.createElement("tr");
+    thead.appendChild(headerRow);
+
+    const headerNames = ["Product Name", "Description", "Price (€)", "Allergens"];
+}
+
+
 function countItems(items) {
     const counts = new Map();
     for (const item of items) {
@@ -102,17 +117,4 @@ function formatItemCounts(counts) {
         text += `${name}: ${count}`;
     }
     return text;
-}
-
-export const menuComponent = (menu) => {
-    const table = document.createElement("table");
-    table.classList.add("menu-table");
-
-    const thead = document.createElement("thead");
-    table.appendChild(thead);
-
-    const headerRow = document.createElement("tr");
-    thead.appendChild(headerRow);
-
-    const headerNames = ["Pizza Name", "Description", "Price (€)", "Allergens"];
 }
