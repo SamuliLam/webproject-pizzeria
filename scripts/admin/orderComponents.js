90a27e4c0788c39cbe7fea417cf4011be4ff36b1
Samuli Lamminmäki <36449245+SamuliLam@users.noreply.github.com>
2024-05-02 17:02:50 +0300

    code refactoring

--- scripts/admin/orderComponents.js
+++ scripts/admin/orderComponents.js
@@ -117,20 +117,6 @@ function createTable(orders, keys, title, headers) {
 }
 
 
-export const menuComponent = (menu) => {
-    const table = document.createElement("table");
-    table.classList.add("menu-table");
-
-    const thead = document.createElement("thead");
-    table.appendChild(thead);
-
-    const headerRow = document.createElement("tr");
-    thead.appendChild(headerRow);
-
-    const headerNames = ["Product Name", "Description", "Price (€)", "Allergens"];
-}
-
-
 function countItems(items) {
     const counts = new Map();
     for (const item of items) {
